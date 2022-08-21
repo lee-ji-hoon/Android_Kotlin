c2c0fa0010e072e07656714fd2010a44479d4b49
lee-ji-hoon <dlwlgns1240@naver.com>
2022-08-21 19:56:03 +0900

    Feat: 섹션1. toast 위치 수정

--- the-big-nerd-ranch-guide-4nd/geo-quiz/app/src/main/java/com/guide/geoGuiz/MainActivity.kt
+++ the-big-nerd-ranch-guide-4nd/geo-quiz/app/src/main/java/com/guide/geoGuiz/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.guide.geoGuiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.Button
+import android.view.Gravity
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.guide.geoGuiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,20 +18,25 @@ class MainActivity : AppCompatActivity() {
         val btnTrue = mainBinding.btnTrue
         btnTrue.setOnClickListener {
             Log.d("btn", "true btn 작동")
-            Toast.makeText(
+            val toast = Toast.makeText(
                 this, // 매개변수 Context는 일반적으로 Activity의 인스턴스이다. 즉 Activity는 Context의 서브 클래스라는 것을 알 수 있다.
                 R.string.correct_toast,
                 Toast.LENGTH_SHORT // 문자열을 얼마나 오래 보여줄지 정하는 것
-            ).show() // 문자열 호출해서 화면에 보이게 해줌
+            )
+            toast.setGravity(Gravity.TOP, 200, 200)
+            toast.show() // 문자열 호출해서 화면에 보이게 해줌
         }
         val btnFalse = mainBinding.btnFalse
         btnFalse.setOnClickListener {
             Log.d("btn", "false btn 작동")
-            Toast.makeText(
+            val toast = Toast.makeText(
                 this,
                 R.string.inCorrect_toast,
                 Toast.LENGTH_SHORT
-            ).show()
+            )
+            // api 28 까지만 위치 조정 가능
+            toast.setGravity(Gravity.TOP,200, 200)
+            toast.show()
         }
     }
 }
