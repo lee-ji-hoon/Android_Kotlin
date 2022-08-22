90d014f30b0b07dc9bc1c654312c6690b6b0ebca
lee-ji-hoon <dlwlgns1240@naver.com>
2022-08-22 17:32:19 +0900

    Feat: 섹션4. ViewModel 추가

--- the-big-nerd-ranch-guide-4nd/geo-quiz/app/src/main/java/com/guide/geoGuiz/MainActivity.kt
+++ the-big-nerd-ranch-guide-4nd/geo-quiz/app/src/main/java/com/guide/geoGuiz/MainActivity.kt
@@ -1,68 +1,81 @@
 package com.guide.geoGuiz
 
-import android.nfc.Tag
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.guide.geoGuiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding // 컴파일 시점에 초기화 되는 것은 불가능하기 때문에 lateinit
     private val correctQuestions = mutableSetOf<Int>()
-    private var currentIndex = 0
     private val TAG = "MainActivity"
 
-    // TODO 모델 데이터를 저장하는 더 좋은 방법이 있지만 우선은 간단하게 List로 생성해서 사용하기
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true),
-    )
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProvider(this)[QuizViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "create")
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
-        val btnTrue = mainBinding.btnTrue
-        val btnFalse = mainBinding.btnFalse
-        val btnNext = mainBinding.btnNext
-        val btnPrev = mainBinding.btnPrevious
-        val tvQuestion = mainBinding.tvQuestion
-
-        updateQuestion(tvQuestion)
-        btnTrue.setOnClickListener {
+        updateQuestion()
+        mainBinding.btnTrue.setOnClickListener {
             checkAnswer(true)
         }
 
-        btnFalse.setOnClickListener {
+        mainBinding.btnFalse.setOnClickListener {
             checkAnswer(false)
         }
 
-        btnNext.setOnClickListener {
-            updateNextQuestion(tvQuestion)
+        mainBinding.btnNext.setOnClickListener {
+            updateNextQuestion()
         }
 
         // 챌린지 1. TextView에 리스너추가
-        tvQuestion.setOnClickListener {
-            updateNextQuestion(tvQuestion)
+        mainBinding.tvQuestion.setOnClickListener {
+            updateNextQuestion()
         }
 
         // 챌린지 2. PREVIOUS 버튼 추가
-        btnPrev?.setOnClickListener {
-            updatePrevQuestion(tvQuestion)
+        mainBinding.btnPrevious?.setOnClickListener {
+            updatePrevQuestion()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "start")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "resume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "pause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "stop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "destroy")
     }
 
     /**
      * 이미 푼 문제인지 확인하는 함수
      */
     private fun isOverlapQuestion() {
-        val currentQuestionId = questionBank[currentIndex].textResId
+        val currentQuestionId = quizViewModel.currentQuestionId
         when (correctQuestions.contains(currentQuestionId)) {
             // stack에 존재한다면 false -> 버튼 클릭 못하게 바꿈
             true -> changedButtonEnabled(false)
@@ -81,39 +94,43 @@ class MainActivity : AppCompatActivity() {
     /**
      * 이전 질문으로 돌아가기
      */
-    private fun updatePrevQuestion(tvQuestion: TextView) {
-        if (currentIndex == 0) currentIndex = questionBank.size - 1
-        else currentIndex -= 1
-        updateQuestion(tvQuestion)
+    private fun updatePrevQuestion() {
+        if (quizViewModel.getCurrentIndex == 0) quizViewModel.moveToLast()
+        else quizViewModel.moveToPrev()
+        updateQuestion()
     }
 
     /**
      * 다음 질문으로 가기
      */
-    private fun updateNextQuestion(tvQuestion: TextView) {
-        currentIndex = (currentIndex + 1) % questionBank.size
-        updateQuestion(tvQuestion)
+    private fun updateNextQuestion() {
+        Log.d(TAG, "updateNextQuestion | 클릭 전 question : ${quizViewModel.currentQuestionId}, ${quizViewModel.getCurrentIndex}")
+        quizViewModel.moveToNext()
+        Log.d(TAG, "updateNextQuestion | 클릭 후 question : ${quizViewModel.currentQuestionId}, ${quizViewModel.getCurrentIndex}")
+        updateQuestion()
     }
 
     /**
      * 질문 업데이트
      */
-    private fun updateQuestion(tvQuestion: TextView) {
+    private fun updateQuestion() {
         // 챌린지 1. 정답 맞춘 문제를 건너띄기
         isOverlapQuestion()
-        val questionTextResId = questionBank[currentIndex].textResId
-        tvQuestion.setText(questionTextResId)
+        val questionTextResId = quizViewModel.currentQuestionId
+        Log.d(TAG, "updateQuestion | 현재 question : ${questionTextResId}")
+        mainBinding.tvQuestion.setText(questionTextResId)
     }
 
     /**
      * 정답 확인
      */
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
+        Log.d(TAG, "checkAnswer | 현재 question : ${quizViewModel.currentQuestionId}")
         val messageResId = when (userAnswer == correctAnswer) {
             // 챌린지 2. 점수 보여주기. -> 마지막 문제인지 확인하고 마지막이면 정답 체크 후 점수를 백분율로 보여주기.
             true -> {
-                correctQuestions.add(questionBank[currentIndex].textResId)
+                correctQuestions.add(quizViewModel.currentQuestionId)
                 changedButtonEnabled(false)
                 R.string.correct_toast
             }
@@ -128,9 +145,14 @@ class MainActivity : AppCompatActivity() {
      * 마지막 문제에서 몇 개 맞췄는지 확인
      */
     private fun checkLastQuestion() {
-        if (currentIndex == questionBank.size - 1) {
-            val ratioCorrectQuestion = ((correctQuestions.size.toDouble() / questionBank.size.toDouble()) * 100.0).toInt()
-            Toast.makeText(this, getString(R.string.ratio_correct, ratioCorrectQuestion), Toast.LENGTH_SHORT).show()
-        } else updateNextQuestion(mainBinding.tvQuestion)
+        if (quizViewModel.getCurrentIndex == quizViewModel.questionBoxSize - 1) {
+            val ratioCorrectQuestion =
+                ((correctQuestions.size.toDouble() / quizViewModel.questionBoxSize.toDouble()) * 100.0).toInt()
+            Toast.makeText(
+                this,
+                getString(R.string.ratio_correct, ratioCorrectQuestion),
+                Toast.LENGTH_SHORT
+            ).show()
+        } else updateNextQuestion()
     }
 }
---
+++ the-big-nerd-ranch-guide-4nd/geo-quiz/app/src/main/java/com/guide/geoGuiz/QuizViewModel.kt
@@ -0,0 +1,38 @@
+package com.guide.geoGuiz
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+/**
+ * @author jihoon
+ * @email dlwlgns1240@gmail.com
+ * @created 2022/08/22
+ * @desc
+ */
+private const val TAG = "QuizViewModel"
+
+class QuizViewModel : ViewModel() {
+    private var currentIndex = 0
+
+    // TODO 모델 데이터를 저장하는 더 좋은 방법이 있지만 우선은 간단하게 List로 생성해서 사용하기
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true),
+    )
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+    val currentQuestionId: Int
+        get() = questionBank[currentIndex].textResId
+    val getCurrentIndex
+        get() = currentIndex
+    val questionBoxSize
+        get() = questionBank.size
+    fun moveToLast() { currentIndex = questionBoxSize - 1 }
+    fun moveToNext() { currentIndex = (currentIndex + 1) % questionBank.size}
+    fun moveToPrev() { currentIndex-- }
+}
