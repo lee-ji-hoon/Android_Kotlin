80d38b2cb0635fd208eee5acc0430110c0dc71fc
lee-ji-hoon <dlwlgns1240@naver.com>
2022-08-24 14:34:09 +0900

    Feat: 프로그래먼트 호스팅

--- the-big-nerd-ranch-guide-4nd/criminal-intent/app/src/main/java/com/guide/criminalIntent/MainActivity.kt
+++ the-big-nerd-ranch-guide-4nd/criminal-intent/app/src/main/java/com/guide/criminalIntent/MainActivity.kt
@@ -2,10 +2,22 @@ package com.guide.criminalIntent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.guide.criminalIntent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currentFragment == null){
+            val fragment = CrimeFragment()
+            supportFragmentManager.beginTransaction() // FragmentTransaction의 인스턴스 생성 반환, fluent interface 를 사용
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
 }
