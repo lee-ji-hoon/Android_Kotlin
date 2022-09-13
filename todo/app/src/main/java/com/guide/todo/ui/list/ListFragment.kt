bac527ddebaaaf7ecc536cb264227cac67391b05
lee-ji-hoon <dlwlgns1240@naver.com>
2022-09-13 22:32:04 +0900

    Feat: addFragment

--- todo/app/src/main/java/com/guide/todo/ui/list/ListFragment.kt
+++ todo/app/src/main/java/com/guide/todo/ui/list/ListFragment.kt
@@ -5,15 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.guide.todo.R
+import com.guide.todo.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
-
+    private lateinit var binding: FragmentListBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+    ): View {
+        binding = FragmentListBinding.inflate(inflater, container, false)
+        binding.btnFloatingAction.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+        return binding.root
     }
 }
