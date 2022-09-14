42e73fd1e91073de66c2a15ce86796a9ab150cbc
lee-ji-hoon <dlwlgns1240@naver.com>
2022-09-14 23:16:02 +0900

    Feat: init android room with view

---
+++ codelabs/room-with-view/app/src/main/java/com/guide/room_with_view/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.guide.room_with_view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
