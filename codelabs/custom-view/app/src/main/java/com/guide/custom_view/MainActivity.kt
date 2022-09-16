f505d87b6971b78fb7119b8a11deb301cb9e7cd8
lee-ji-hoon <dlwlgns1240@naver.com>
2022-09-16 19:39:54 +0900

    Init : codelabs custom-view

---
+++ codelabs/custom-view/app/src/main/java/com/guide/custom_view/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.guide.custom_view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
