259882d7549d1ec534de8fabadab288ce25f9da7
lee-ji-hoon <dlwlgns1240@naver.com>
2022-09-23 12:15:06 +0900

    init: rectangle-canvas

---
+++ etc/observer/app/src/main/java/com/android/observer/Observers.kt
@@ -0,0 +1,11 @@
+package com.android.observer
+
+/**
+* @author jihoon
+* @email dlwlgns1240@gmail.com
+* @created 2022/09/21
+* @desc
+*/
+
+class Observers {
+}
---
+++ etc/observer/app/src/main/java/com/android/observer/Publisher.kt
@@ -0,0 +1,4 @@
+package com.android.observer
+
+interface Publisher {
+}
