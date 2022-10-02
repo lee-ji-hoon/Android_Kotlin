45ef50614d74af69285d124a6a1ad38e207f071e
lee-ji-hoon <dlwlgns1240@naver.com>
2022-10-02 14:54:40 +0900

    Init: permission

---
+++ etc/permission/app/src/main/java/com/android/permission/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.android.permission
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ etc/serialization/app/src/main/java/com/android/serialization/ParcelableShareData.kt
@@ -0,0 +1,11 @@
+package com.android.serialization
+
+/**
+* @author jihoon
+* @email dlwlgns1240@gmail.com
+* @created 2022/09/28
+* @desc
+*/
+
+class ParcelableShareData {
+}
